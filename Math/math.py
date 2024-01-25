b54374e46f556c75a107284706a5d16fba6e9acd
WestraTech <02gabriel.westra@gulllakecs.org>
2024-01-25 09:16:38 -0500

    Math Relocated

---
+++ Math/math.py
@@ -0,0 +1,12 @@
+#Programmer: Gabriel Westra
+#Program: Math
+#Date: 1.10.24
+
+print(9 * 9) #Multiply
+print(9 + 9) #Add
+print(9 - 6) #Subtract
+print(9 / 3) #Divide
+print(20 ** 3) #Exponent
+print(50 % 6) #Modulas
+print(50 / 6) #Division With Remainder; Float
+print(50 // 6) #No Remainder
